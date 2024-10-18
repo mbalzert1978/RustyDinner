fe4016820f2728b35c53b5b0e8b3e8a3ae6b8991
Markus Iorio <mbalzert@gmail.com>
2024-10-18 22:58:51 +0200

    fast webserver

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,41 @@
 mod error;
 mod prelude;
 
+use axum::extract::Path;
+use axum::response::Json;
+use axum::routing::get;
+use axum::Router;
 pub use prelude::*;
+use serde::Serialize;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Serialize)]
+struct Weather {
+    city: String,
+    temperature: f32,
+    condition: String,
+}
+
+async fn get_weather(Path(city): Path<String>) -> Json<Weather> {
+    let weather = Weather {
+        city,
+        temperature: 22.0,
+        condition: "Sunny".to_string(),
+    };
+
+    Json(weather)
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().route("/weather/:city", get(get_weather));
+
+    let listener = tokio::net::TcpListener::bind(format!("{}:{}", "localhost", "8000"))
+        .await
+        .expect("Could not bind to address");
+
+    axum::serve(listener, app)
+        .await
+        .expect("Could not start server");
+
+    Ok(())
 }
